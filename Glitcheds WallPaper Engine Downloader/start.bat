e3295c3e1cb5da7ce20e96c1014517170b94eaa3
Glitched Da Kitty Cat School Account <178820833+GlitchedDaKittyCatSchoolaccount@users.noreply.github.com>
2025-07-27 05:59:18 -0500

    Delete Glitcheds WallPaper Engine Downloader/start.bat

--- Glitcheds WallPaper Engine Downloader/start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Wallpaper Engine Workshop Downloader
-echo Starting Wallpaper Engine Workshop Downloader...
-echo.
-pip install -r requirements.txt
-echo.
-python WE WallPaper Downloader.py
-pause
